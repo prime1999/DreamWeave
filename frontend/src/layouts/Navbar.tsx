765ba8d19cd6f53c6df55a90b6aedffc1415c717
prime <primetechnologies02@gmail.com>
2023-11-14 19:14:58 +0000

    added the navbar UI

--- frontend/src/layouts/Navbar.tsx
+++ frontend/src/layouts/Navbar.tsx
@@ -1,11 +1,103 @@
+import { HiMagnifyingGlass } from "react-icons/hi2";
+import { TbShoppingCartPlus } from "react-icons/tb";
+import { MdPersonPin } from "react-icons/md";
 import logo from "../assets/images/logo.png";
-
+import { Link } from "react-router-dom";
+import laptop from "../assets/images/d1.jpg";
+import phone from "../assets/images/d2.jpg";
+import watch from "../assets/images/d9.webp";
+import {
+	NavigationMenu,
+	NavigationMenuContent,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	NavigationMenuTrigger,
+} from "../components/ui/navigation-menu";
 const Navbar = () => {
 	return (
-		<div>
-			<div className="w-12 flex items-end py-4">
-				<img src={logo} alt="logo" />
-				<h1 className="font-kenia text-xl mb-1 ml-1 text-black">DREAMWEAVE</h1>
+		<div className="flex items-center justify-between px-8">
+			<Link to="/" className="flex items-center py-4 font-poppins">
+				<img className="w-12" src={logo} alt="logo" />
+				<h1 className="font-kenia text-xl ml-1 text-black">DREAMWEAVE</h1>
+			</Link>
+			<div className="flex items-center justify-between">
+				<div className="relative mx-2">
+					<NavigationMenu>
+						<NavigationMenuList className="">
+							<NavigationMenuItem>
+								<NavigationMenuTrigger className="font-poppins text-sm font-semibold text-blue hover:bg-transparent hover:text-blue">
+									Categories
+								</NavigationMenuTrigger>
+								<NavigationMenuContent>
+									<NavigationMenuLink>
+										<div className="p-4 border-none bg-transparent w-96">
+											<h6 className="font-poppins text-sm font-bold mb-2">
+												Popular Categories
+											</h6>
+											<ul className="w-full font-semibold">
+												<hr className="mb-4" />
+												<li className="flex">
+													<img className="w-24" src={laptop} alt="" />
+													<Link className="bg-gray-200 p-2 ml-2 w-full" to="/">
+														Laptops and computer components
+													</Link>
+												</li>
+												<li className="flex mt-2">
+													<img className="w-24" src={phone} alt="" />
+													<Link className="bg-gray-200 p-2 ml-2 w-full" to="/">
+														Smartpones and accesories
+													</Link>
+												</li>
+												<li className="flex mt-2">
+													<img className="w-24" src={watch} alt="" />
+													<Link className="bg-gray-200 p-2 ml-2 w-full" to="/">
+														Wearable tech
+													</Link>
+												</li>
+											</ul>
+										</div>
+									</NavigationMenuLink>
+								</NavigationMenuContent>
+							</NavigationMenuItem>
+						</NavigationMenuList>
+					</NavigationMenu>
+				</div>
+				<div className="font-poppins text-sm font-semibold text-blue ">
+					<ul className="flex items-center justify-between">
+						<li>
+							<Link to="/">Deals</Link>
+						</li>
+						<li className="mx-6">
+							<Link to="/">What's new</Link>
+						</li>
+						<li>
+							<Link to="/">Delivery</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div>
+				<form className="relative">
+					<input
+						type="text"
+						placeholder="search.."
+						className="bg-gray-100 px-4 py-1 rounded-3xl pr-8 w-72 focus:outline-none"
+					/>
+					<HiMagnifyingGlass className="absolute top-2 right-3" />
+				</form>
+			</div>
+			<div className="flex items-center justify-between">
+				<Link to="/" className="flex items-center mr-4">
+					<MdPersonPin className="w-18 text-3xl text-blue" />
+					<p className="font-poppins text-black text-sm font-semibold">
+						Account
+					</p>
+				</Link>
+				<Link to="/" className="flex items-center">
+					<TbShoppingCartPlus className="w-18 text-2xl text-blue" />
+					<p className="font-poppins text-black text-sm font-semibold">Cart</p>
+				</Link>
 			</div>
 		</div>
 	);
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,20 +1,85 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+	darkMode: ["class"],
+	content: [
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
+	],
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
 			fontFamily: {
+				kenia: ["kenia", "sans-serif"],
 				poppins: ["poppins", "sans-serif"],
 				cour: ["Courgette", "cursive"],
-				kenia: ["Kenia", "sans-serif"],
 			},
 			colors: {
 				black: "#2F414F",
 				blue: "#3894A3",
-				light: "#FIF1EF",
+				light: "#F1F1EF",
 				other: "#C7DAD4",
+				border: "hsl(var(--border))",
+				input: "hsl(var(--input))",
+				ring: "hsl(var(--ring))",
+				background: "hsl(var(--background))",
+				foreground: "hsl(var(--foreground))",
+				primary: {
+					DEFAULT: "hsl(var(--primary))",
+					foreground: "hsl(var(--primary-foreground))",
+				},
+				secondary: {
+					DEFAULT: "hsl(var(--secondary))",
+					foreground: "hsl(var(--secondary-foreground))",
+				},
+				destructive: {
+					DEFAULT: "hsl(var(--destructive))",
+					foreground: "hsl(var(--destructive-foreground))",
+				},
+				muted: {
+					DEFAULT: "hsl(var(--muted))",
+					foreground: "hsl(var(--muted-foreground))",
+				},
+				accent: {
+					DEFAULT: "hsl(var(--accent))",
+					foreground: "hsl(var(--accent-foreground))",
+				},
+				popover: {
+					DEFAULT: "hsl(var(--popover))",
+					foreground: "hsl(var(--popover-foreground))",
+				},
+				card: {
+					DEFAULT: "hsl(var(--card))",
+					foreground: "hsl(var(--card-foreground))",
+				},
+			},
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			keyframes: {
+				"accordion-down": {
+					from: { height: 0 },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: 0 },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require("tailwindcss-animate")],
 };

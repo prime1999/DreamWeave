cc9a216792a52a2238f8fa717431b11a992e8655
prime <primetechnologies02@gmail.com>
2023-11-17 06:16:43 +0000

    started building the proucts UI

--- frontend/src/components/ProductsComponent/ProductCard.tsx
+++ frontend/src/components/ProductsComponent/ProductCard.tsx
@@ -1,5 +1,46 @@
-const ProductCard = () => {
-	return <div>ProductCard</div>;
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { ProductType } from "@/DataTypes/ProductType";
+
+type Props = {
+	product: ProductType;
+};
+
+const ProductCard = ({ product }: Props) => {
+	return (
+		<Card className="w-[300px]">
+			<CardHeader className="w-full">
+				<div className="w-full h-[250px]">
+					<img
+						className="w-full max-h-[250px]"
+						src={product.image}
+						alt="product's image"
+					/>
+				</div>
+				<div className="flex justify-between">
+					<CardTitle className="font-poppins text-md truncate">
+						{product.name}
+					</CardTitle>
+					<p className="font-semibold">${product.price}</p>
+				</div>
+				<CardDescription className="truncate font-semibold text-blue">
+					{product.description}
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<p>Card Content</p>
+			</CardContent>
+			<CardFooter>
+				<p>Card Footer</p>
+			</CardFooter>
+		</Card>
+	);
 };
 
 export default ProductCard;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,23 @@
 import Sorting from "../components/layouts/Sorting";
 import Hero from "../components/layouts/Hero";
 import { useGetProductsQuery } from "@/slices/ProductSlice";
+import ProductCard from "@/components/ProductsComponent/ProductCard";
 
 const HomePage = () => {
 	const { data } = useGetProductsQuery({} as any);
 	return (
 		<div>
 			<Hero />
-			<div className="container mx-auto my-8">
+			<div className="container mx-auto my-8 w-full">
 				<Sorting />
-				{data && <h1>{data[0].name}</h1>}
+
+				{data && (
+					<div className="grid grid-cols-4 gap-2 mt-8">
+						{data.map((product) => (
+							<ProductCard key={product._id} product={product} />
+						))}
+					</div>
+				)}
 			</div>
 		</div>
 	);
